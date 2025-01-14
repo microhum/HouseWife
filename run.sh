2837f24436020314d6396da44ac69ff0720434cf
microhum <Guntee12123@gmail.com>
2025-01-15 00:02:09 +0700

    retry for hosting #17

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
-
-(
 if [[ "$OS" == "Windows_NT" ]]; then
     python tools/loadenv_yaml.py
 else
     python3 tools/loadenv_yaml.py
 fi
-(cd lavalink && java -jar Lavalink.jar) & (sleep 10 &&
+(cd lavalink && java -jar Lavalink.jar) &
+sleep 10
 python3 src/main.py
-)
 
